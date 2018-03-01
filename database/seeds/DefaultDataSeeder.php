548f81bcf78f0eebd94ce268c5b8b556b82403b6
Halil İbrahim ceylan <hibrahim.ceylan@std.hku.edu.tr>
2018-03-01 22:05:00 +0300

    create default data seeder

---
+++ database/seeds/DefaultDataSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\ConnectionTag;
+class DefaultDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      ConnectionTag::create['action_url' => 'http://ridvandayanc.com'];
+      ConnectionTag::create['action_url' => 'http://haliliceylan.com'];
+    }
+}

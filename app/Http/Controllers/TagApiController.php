aaaa2e319b45f9bd1af52c5e01262c31daab444d
Halil İbrahim ceylan <hibrahim.ceylan@std.hku.edu.tr>
2018-03-01 22:57:57 +0300

    integrate browser detect system

--- app/Http/Controllers/TagApiController.php
+++ app/Http/Controllers/TagApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\ConnectionTag;
 use App\Action;
+use Browser;
 
 class TagApiController extends Controller
 {
@@ -14,6 +15,16 @@ class TagApiController extends Controller
         $action = new Action;
         $action->user_agent = $request->header('User-Agent');
         $action->ip_address = $request->ip();
+        $action->browser_name = Browser::browserName();
+        $action->browser_family = Browser::browserFamily();
+        $action->browser_version = Browser::browserVersion();
+        $action->browser_engine = Browser::browserEngine();
+        $action->platform_name = Browser::platformName();
+        $action->platform_family = Browser::platformFamily();
+        $action->platform_version = Browser::platformVersion();
+        $action->device_family = Browser::deviceFamily();
+        $action->device_model = Browser::deviceModel();
+        $action->mobile_grade = Browser::mobileGrade();
         $action->connection_tag_id = $connectionTag->id;
         $action->save();
         //redirect user
--- database/migrations/2018_03_01_184907_create_actions_table.php
+++ database/migrations/2018_03_01_184907_create_actions_table.php
@@ -17,6 +17,16 @@ class CreateActionsTable extends Migration
             $table->increments('id');
             $table->integer('connection_tag_id');
             $table->text('user_agent');
+            $table->text('browser_name');
+            $table->text('browser_family');
+            $table->text('browser_version');
+            $table->text('browser_engine');
+            $table->text('platform_name');
+            $table->text('platform_family');
+            $table->text('platform_version');
+            $table->text('device_family');
+            $table->text('device_model');
+            $table->text('mobile_grade');
             $table->text('ip_address');
             $table->timestamps();
         });

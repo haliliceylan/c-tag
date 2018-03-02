b1d9dc20e4396128ec1d397a11b7e111704b4df7
Halil İbrahim ceylan <hibrahim.ceylan@std.hku.edu.tr>
2018-03-02 15:27:40 +0300

    add default users

--- database/seeds/DefaultDataSeeder.php
+++ database/seeds/DefaultDataSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\ConnectionTag;
+
 class DefaultDataSeeder extends Seeder
 {
     /**
@@ -11,7 +12,31 @@ class DefaultDataSeeder extends Seeder
      */
     public function run()
     {
-      ConnectionTag::create(['action_url' => 'http://ridvandayanc.com']);
-      ConnectionTag::create(['action_url' => 'http://haliliceylan.com']);
+        ConnectionTag::create(['action_url' => 'http://ridvandayanc.com']);
+        ConnectionTag::create(['action_url' => 'http://itucekirdek.com/']);
+        Sentinel::registerAndActivate([
+            'email'    => 'ridvan@peakloop.com',
+            'password' => '12345',
+            'first_name' => 'Rıdvan',
+            'last_name' => 'DAYANÇ'
+        ]);
+        Sentinel::registerAndActivate([
+            'email'    => 'erdenay@peakloop.com',
+            'password' => '12345',
+            'first_name' => 'Erdenay',
+            'last_name' => 'ÖZKANLI'
+        ]);
+        Sentinel::registerAndActivate([
+            'email'    => 'halil@peakloop.com',
+            'password' => '12345',
+            'first_name' => 'Halil İbrahim',
+            'last_name' => 'CEYLAN'
+        ]);
+        Sentinel::registerAndActivate([
+            'email'    => 'nihat@peakloop.com',
+            'password' => '12345',
+            'first_name' => 'Nihat Yılmaz',
+            'last_name' => 'ŞİMŞEK'
+        ]);
     }
 }

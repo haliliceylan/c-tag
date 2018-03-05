453c622ae8d308b1a3577acd1d38b4166659fe90
Halil İbrahim ceylan <hibrahim.ceylan@std.hku.edu.tr>
2018-03-05 09:03:24 +0300

    frontend label is fixed

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>{{env('APP_NAME')}} | @yield('page_name','Analiz Sayfası')</title>
+  <title>{{'PeakLoop'}} | {{env('APP_NAME')}}</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->

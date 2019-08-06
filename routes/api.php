8d3f1a425d39c020dda0bd1de1877912d72f69ca
Halil İbrahim ceylan <hibrahim.ceylan@std.hku.edu.tr>
2019-08-07 00:08:00 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,5 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 Route::get('/tag/{connectionTag}/show','Api\ConnectionTagController@show')->name('show_tag');
 Route::get('/tag/{connectionTag}/{qccode?}', 'TagApiController@runAction')->name('tag_action');
